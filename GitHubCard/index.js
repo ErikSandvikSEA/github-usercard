fd9dfd4367e297f63a5260686eb075ee42381db5
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-09 13:39:00 -0700

    added followers to followersArray

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,314 @@
+const followersObjectArray = [
+  {
+    "login": "nicholas-myers",
+    "id": 61495108,
+    "node_id": "MDQ6VXNlcjYxNDk1MTA4",
+    "avatar_url": "https://avatars0.githubusercontent.com/u/61495108?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/nicholas-myers",
+    "html_url": "https://github.com/nicholas-myers",
+    "followers_url": "https://api.github.com/users/nicholas-myers/followers",
+    "following_url": "https://api.github.com/users/nicholas-myers/following{/other_user}",
+    "gists_url": "https://api.github.com/users/nicholas-myers/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/nicholas-myers/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/nicholas-myers/subscriptions",
+    "organizations_url": "https://api.github.com/users/nicholas-myers/orgs",
+    "repos_url": "https://api.github.com/users/nicholas-myers/repos",
+    "events_url": "https://api.github.com/users/nicholas-myers/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/nicholas-myers/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  {
+    "login": "Kandelonius",
+    "id": 51350138,
+    "node_id": "MDQ6VXNlcjUxMzUwMTM4",
+    "avatar_url": "https://avatars3.githubusercontent.com/u/51350138?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/Kandelonius",
+    "html_url": "https://github.com/Kandelonius",
+    "followers_url": "https://api.github.com/users/Kandelonius/followers",
+    "following_url": "https://api.github.com/users/Kandelonius/following{/other_user}",
+    "gists_url": "https://api.github.com/users/Kandelonius/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/Kandelonius/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/Kandelonius/subscriptions",
+    "organizations_url": "https://api.github.com/users/Kandelonius/orgs",
+    "repos_url": "https://api.github.com/users/Kandelonius/repos",
+    "events_url": "https://api.github.com/users/Kandelonius/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/Kandelonius/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  {
+    "login": "wclausen",
+    "id": 3293136,
+    "node_id": "MDQ6VXNlcjMyOTMxMzY=",
+    "avatar_url": "https://avatars1.githubusercontent.com/u/3293136?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/wclausen",
+    "html_url": "https://github.com/wclausen",
+    "followers_url": "https://api.github.com/users/wclausen/followers",
+    "following_url": "https://api.github.com/users/wclausen/following{/other_user}",
+    "gists_url": "https://api.github.com/users/wclausen/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/wclausen/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/wclausen/subscriptions",
+    "organizations_url": "https://api.github.com/users/wclausen/orgs",
+    "repos_url": "https://api.github.com/users/wclausen/repos",
+    "events_url": "https://api.github.com/users/wclausen/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/wclausen/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  {
+    "login": "Owlspec3086",
+    "id": 49787190,
+    "node_id": "MDQ6VXNlcjQ5Nzg3MTkw",
+    "avatar_url": "https://avatars2.githubusercontent.com/u/49787190?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/Owlspec3086",
+    "html_url": "https://github.com/Owlspec3086",
+    "followers_url": "https://api.github.com/users/Owlspec3086/followers",
+    "following_url": "https://api.github.com/users/Owlspec3086/following{/other_user}",
+    "gists_url": "https://api.github.com/users/Owlspec3086/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/Owlspec3086/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/Owlspec3086/subscriptions",
+    "organizations_url": "https://api.github.com/users/Owlspec3086/orgs",
+    "repos_url": "https://api.github.com/users/Owlspec3086/repos",
+    "events_url": "https://api.github.com/users/Owlspec3086/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/Owlspec3086/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  {
+    "login": "mcelroyian",
+    "id": 937995,
+    "node_id": "MDQ6VXNlcjkzNzk5NQ==",
+    "avatar_url": "https://avatars2.githubusercontent.com/u/937995?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/mcelroyian",
+    "html_url": "https://github.com/mcelroyian",
+    "followers_url": "https://api.github.com/users/mcelroyian/followers",
+    "following_url": "https://api.github.com/users/mcelroyian/following{/other_user}",
+    "gists_url": "https://api.github.com/users/mcelroyian/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/mcelroyian/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/mcelroyian/subscriptions",
+    "organizations_url": "https://api.github.com/users/mcelroyian/orgs",
+    "repos_url": "https://api.github.com/users/mcelroyian/repos",
+    "events_url": "https://api.github.com/users/mcelroyian/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/mcelroyian/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  {
+    "login": "corbinrobb",
+    "id": 31329271,
+    "node_id": "MDQ6VXNlcjMxMzI5Mjcx",
+    "avatar_url": "https://avatars3.githubusercontent.com/u/31329271?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/corbinrobb",
+    "html_url": "https://github.com/corbinrobb",
+    "followers_url": "https://api.github.com/users/corbinrobb/followers",
+    "following_url": "https://api.github.com/users/corbinrobb/following{/other_user}",
+    "gists_url": "https://api.github.com/users/corbinrobb/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/corbinrobb/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/corbinrobb/subscriptions",
+    "organizations_url": "https://api.github.com/users/corbinrobb/orgs",
+    "repos_url": "https://api.github.com/users/corbinrobb/repos",
+    "events_url": "https://api.github.com/users/corbinrobb/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/corbinrobb/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  {
+    "login": "jonush",
+    "id": 28828354,
+    "node_id": "MDQ6VXNlcjI4ODI4MzU0",
+    "avatar_url": "https://avatars3.githubusercontent.com/u/28828354?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/jonush",
+    "html_url": "https://github.com/jonush",
+    "followers_url": "https://api.github.com/users/jonush/followers",
+    "following_url": "https://api.github.com/users/jonush/following{/other_user}",
+    "gists_url": "https://api.github.com/users/jonush/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/jonush/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/jonush/subscriptions",
+    "organizations_url": "https://api.github.com/users/jonush/orgs",
+    "repos_url": "https://api.github.com/users/jonush/repos",
+    "events_url": "https://api.github.com/users/jonush/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/jonush/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  {
+    "login": "vailspencer",
+    "id": 41238779,
+    "node_id": "MDQ6VXNlcjQxMjM4Nzc5",
+    "avatar_url": "https://avatars1.githubusercontent.com/u/41238779?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/vailspencer",
+    "html_url": "https://github.com/vailspencer",
+    "followers_url": "https://api.github.com/users/vailspencer/followers",
+    "following_url": "https://api.github.com/users/vailspencer/following{/other_user}",
+    "gists_url": "https://api.github.com/users/vailspencer/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/vailspencer/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/vailspencer/subscriptions",
+    "organizations_url": "https://api.github.com/users/vailspencer/orgs",
+    "repos_url": "https://api.github.com/users/vailspencer/repos",
+    "events_url": "https://api.github.com/users/vailspencer/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/vailspencer/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  {
+    "login": "avpimblesr",
+    "id": 22718695,
+    "node_id": "MDQ6VXNlcjIyNzE4Njk1",
+    "avatar_url": "https://avatars3.githubusercontent.com/u/22718695?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/avpimblesr",
+    "html_url": "https://github.com/avpimblesr",
+    "followers_url": "https://api.github.com/users/avpimblesr/followers",
+    "following_url": "https://api.github.com/users/avpimblesr/following{/other_user}",
+    "gists_url": "https://api.github.com/users/avpimblesr/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/avpimblesr/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/avpimblesr/subscriptions",
+    "organizations_url": "https://api.github.com/users/avpimblesr/orgs",
+    "repos_url": "https://api.github.com/users/avpimblesr/repos",
+    "events_url": "https://api.github.com/users/avpimblesr/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/avpimblesr/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  {
+    "login": "cdfallday",
+    "id": 42876707,
+    "node_id": "MDQ6VXNlcjQyODc2NzA3",
+    "avatar_url": "https://avatars2.githubusercontent.com/u/42876707?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/cdfallday",
+    "html_url": "https://github.com/cdfallday",
+    "followers_url": "https://api.github.com/users/cdfallday/followers",
+    "following_url": "https://api.github.com/users/cdfallday/following{/other_user}",
+    "gists_url": "https://api.github.com/users/cdfallday/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/cdfallday/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/cdfallday/subscriptions",
+    "organizations_url": "https://api.github.com/users/cdfallday/orgs",
+    "repos_url": "https://api.github.com/users/cdfallday/repos",
+    "events_url": "https://api.github.com/users/cdfallday/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/cdfallday/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  {
+    "login": "jvaldezdipre",
+    "id": 53051337,
+    "node_id": "MDQ6VXNlcjUzMDUxMzM3",
+    "avatar_url": "https://avatars3.githubusercontent.com/u/53051337?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/jvaldezdipre",
+    "html_url": "https://github.com/jvaldezdipre",
+    "followers_url": "https://api.github.com/users/jvaldezdipre/followers",
+    "following_url": "https://api.github.com/users/jvaldezdipre/following{/other_user}",
+    "gists_url": "https://api.github.com/users/jvaldezdipre/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/jvaldezdipre/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/jvaldezdipre/subscriptions",
+    "organizations_url": "https://api.github.com/users/jvaldezdipre/orgs",
+    "repos_url": "https://api.github.com/users/jvaldezdipre/repos",
+    "events_url": "https://api.github.com/users/jvaldezdipre/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/jvaldezdipre/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  {
+    "login": "SoneThaya",
+    "id": 17104828,
+    "node_id": "MDQ6VXNlcjE3MTA0ODI4",
+    "avatar_url": "https://avatars1.githubusercontent.com/u/17104828?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/SoneThaya",
+    "html_url": "https://github.com/SoneThaya",
+    "followers_url": "https://api.github.com/users/SoneThaya/followers",
+    "following_url": "https://api.github.com/users/SoneThaya/following{/other_user}",
+    "gists_url": "https://api.github.com/users/SoneThaya/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/SoneThaya/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/SoneThaya/subscriptions",
+    "organizations_url": "https://api.github.com/users/SoneThaya/orgs",
+    "repos_url": "https://api.github.com/users/SoneThaya/repos",
+    "events_url": "https://api.github.com/users/SoneThaya/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/SoneThaya/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  {
+    "login": "gabester78",
+    "id": 48218164,
+    "node_id": "MDQ6VXNlcjQ4MjE4MTY0",
+    "avatar_url": "https://avatars0.githubusercontent.com/u/48218164?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/gabester78",
+    "html_url": "https://github.com/gabester78",
+    "followers_url": "https://api.github.com/users/gabester78/followers",
+    "following_url": "https://api.github.com/users/gabester78/following{/other_user}",
+    "gists_url": "https://api.github.com/users/gabester78/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/gabester78/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/gabester78/subscriptions",
+    "organizations_url": "https://api.github.com/users/gabester78/orgs",
+    "repos_url": "https://api.github.com/users/gabester78/repos",
+    "events_url": "https://api.github.com/users/gabester78/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/gabester78/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  {
+    "login": "erictaylor103",
+    "id": 13473000,
+    "node_id": "MDQ6VXNlcjEzNDczMDAw",
+    "avatar_url": "https://avatars2.githubusercontent.com/u/13473000?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/erictaylor103",
+    "html_url": "https://github.com/erictaylor103",
+    "followers_url": "https://api.github.com/users/erictaylor103/followers",
+    "following_url": "https://api.github.com/users/erictaylor103/following{/other_user}",
+    "gists_url": "https://api.github.com/users/erictaylor103/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/erictaylor103/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/erictaylor103/subscriptions",
+    "organizations_url": "https://api.github.com/users/erictaylor103/orgs",
+    "repos_url": "https://api.github.com/users/erictaylor103/repos",
+    "events_url": "https://api.github.com/users/erictaylor103/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/erictaylor103/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  {
+    "login": "ernienettles",
+    "id": 61762647,
+    "node_id": "MDQ6VXNlcjYxNzYyNjQ3",
+    "avatar_url": "https://avatars2.githubusercontent.com/u/61762647?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/ernienettles",
+    "html_url": "https://github.com/ernienettles",
+    "followers_url": "https://api.github.com/users/ernienettles/followers",
+    "following_url": "https://api.github.com/users/ernienettles/following{/other_user}",
+    "gists_url": "https://api.github.com/users/ernienettles/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/ernienettles/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/ernienettles/subscriptions",
+    "organizations_url": "https://api.github.com/users/ernienettles/orgs",
+    "repos_url": "https://api.github.com/users/ernienettles/repos",
+    "events_url": "https://api.github.com/users/ernienettles/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/ernienettles/received_events",
+    "type": "User",
+    "site_admin": false
+  }
+]
+
+
+
+
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-axios.get('https://api.github.com/users/ErikSandvikSEA')
+// axios.get('https://api.github.com/users/ErikSandvikSEA')
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -121,10 +427,26 @@ const buildCard = (githubUsername) => {
 const testResult = buildCard("ErikSandvikSEA")
 
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+
+
+
+
+
+const addCards = () => {
+  followersArray.push('ErikSandvikSEA')
+  followersObjectArray.forEach(follower => {
+    followersArray.push(follower.login)
+  })
+}
+addCards()
+console.log(followersArray)
+// List of LS Instructors Github username's: 
+  // addCards("tetondan")
+  // addCards("justsml")
+  // addCards('dustinmyers')
+  // addCards('justsml')
+  // addCards('luishrd')
+  // addCards('bigknell')
+
+
+
